204fc0516bb4250d004c589c4fe23fd38a55266b
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-08-03 15:09:48 +0300

    Add src/components/ui/Header.tsx

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <h1 className="text-lg">Note App</h1>
+    </header>
+  );
+};
+
+export default Header;
